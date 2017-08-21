273ca87069efecce41b61785776a33938f0df365
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-08-20 23:02:32 -0400

    Possible scope-safe ClozeCard constructor

--- ClozeCard.js
+++ ClozeCard.js
@@ -11,3 +11,18 @@ var ClozeCard = function(text, cloze) {
 };
 
 module.exports = ClozeCard; 
+
+var ClozeCards = function(text, cloze) {
+	if(!(this instanceof ClozeCards)) {
+		return new ClozeCards(text, cloze);
+	}
+	this.cloze = cloze;
+	this.partial = text.replace(cloze,'...');
+	this.fullText = text;
+	this.error = function() {
+		if(!text.includes(cloze)) {
+			console.log('Error: Cloze-deletion does not appear within the full text.');
+			return;
+		};
+	};
+};
